d7eda958160c62d41b1f8c9b727e05276559621d
manmohan841208 <manmohan.4.singh@coforge.com>
2026-05-27 12:28:41 +0530

    next updated

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"RcLtH5aNMBfd+fFnL/eahCpEH/VyTlS67BF4Mf0U4SU=\"\n}"
